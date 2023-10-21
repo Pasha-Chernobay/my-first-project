9fb89832613412fea34d0c25678b8046efa52380
Pavel Chernobay <mr.chernobay@yandex.ru>
2023-10-22 00:27:10 +0300

    RecursiveTask<Long> and ForkJoinPool

--- BiggestFolderFinder/src/Main.java
+++ BiggestFolderFinder/src/Main.java
@@ -1,10 +1,17 @@
 import java.io.File;
+import java.util.concurrent.ForkJoinPool;
 
 public class Main {
     public static void main(String[] args) {
         String folderPath = "/Users/pavelchernobay/skillboxJava";
         File file = new File(folderPath);
-        System.out.println(getFolderSize(file));
+        long start = System.currentTimeMillis();
+        FolderSizeCalculator calculator = new FolderSizeCalculator(file);
+        ForkJoinPool pool = new ForkJoinPool();
+        long size = pool.invoke(calculator);
+        System.out.println(size);
+        //System.out.println(getFolderSize(file));
+        System.out.println(System.currentTimeMillis() - start);
     }
 
     public static long getFolderSize(File folder) {
