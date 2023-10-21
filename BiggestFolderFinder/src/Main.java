556316ef98b76080bc9fad48fbbc1467e373927c
Pavel Chernobay <mr.chernobay@yandex.ru>
2023-10-21 23:17:13 +0300

    getFolderSize in bytes

--- BiggestFolderFinder/src/Main.java
+++ BiggestFolderFinder/src/Main.java
@@ -1,5 +1,21 @@
+import java.io.File;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String folderPath = "/Users/pavelchernobay/skillboxJava";
+        File file = new File(folderPath);
+        System.out.println(getFolderSize(file));
+    }
+
+    public static long getFolderSize(File folder) {
+        long size = 0;
+        if (folder.isFile()) {
+            return folder.length();
+        }
+        File[] files = folder.listFiles();
+        for (File file : files) {
+            size += getFolderSize(file);
+        }
+        return size;
     }
 }
